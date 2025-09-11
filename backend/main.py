07e4e5a2a0ad1233c5de0b6e1a0a68ac16091b7e
stanley-agwu <stanleyagwu2016@gmail.com>
2025-09-11 18:54:57 +0200

    feat(backend): created base class, db set up

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
+from core.config import settings
+from db.session import engine
+from db.base_class import Base
 
-app = FastAPI(title='buzzBlog')
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_TITLE, version=settings.PROJECT_VERSION)
+    create_tables()
+    return app
+
+app = start_application()
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
